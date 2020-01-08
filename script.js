952e1d37762982e5940ad0622657926725b22f16
Jiamin Lu <lujiamin@mail.ustc.edu.cn>
2020-01-09 01:10:24 +0800

    It's working!

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
   const document = window.document;
   const console = window.console;
   const $ = window.jQuery;
+  var results;
 
   // LeetCode page writes to URL, have to restore it with trick
   // http://perrymitchell.net/article/restoring-overridden-window-and-document-methods-with-archetype/
@@ -14,17 +15,16 @@
     let URL = iframe.contentWindow.URL;
     return URL;
   })(document);
+  window.URL = URL;
 
   // Load dependencies
   if (typeof saveAs === "undefined") {
     $.getScript("https://cdn.jsdelivr.net/g/filesaver.js");
   }
 
-  const URL_GraphQL = "https://leetcode.com/graphql"
-
   const queryGraphQL = function (operationName, variables, query) {
     return $.post({
-      url: URL_GraphQL,
+      url: "https://leetcode.com/graphql",
       data: JSON.stringify({
         operationName: operationName,
         variables: variables,
@@ -35,9 +35,18 @@
     });
   };
 
-  const saveResult = function (data) {
-    let blob = new Blob([JSON.stringify(data)], {type: "application/json"});
-    saveAs(blob, "result.json");
+  const filterResult = function () {
+    results = results.filter(item => item['solutions'].length !== 0);
+  };
+
+  const saveResult = function () {
+    let blob = new Blob([JSON.stringify(results)], {type: "application/json"});
+    window.saveAs(blob, "result.json");
+  };
+
+  const getAllQuestions = function () {
+    return $.get("https://leetcode.com/api/problems/all/").then(
+      data => JSON.parse(data)['stat_status_pairs'].map(x => x['stat']['question__title_slug']));
   };
 
   const getQuestion = function (titleSlug) {
@@ -45,7 +54,58 @@
     return queryGraphQL("questionData", {titleSlug: titleSlug}, "query questionData($titleSlug: String!) { question(titleSlug: $titleSlug) { questionId titleSlug solution { id content } } }");
   };
 
-  getQuestion("two-sum").then(function (data) {
-    window.item = data;
+  const parseSolutionURLs = function (text) {
+    if (text == undefined)
+      return [];
+    let result = text.match(/(?<=https:\/\/leetcode\.com\/playground\/)\w+(?=\/shared)/g);
+    if (result === null)
+      return [];
+    return result;
+  };
+
+  const getSolution = function (uuid) {
+    return queryGraphQL("fetchPlayground", {uuid: uuid}, "query fetchPlayground($uuid: String!) { allPlaygroundCodes(uuid: $uuid) { code langSlug } }");
+  };
+
+  // Start crawling!
+  results = [];
+  let promises = [];
+  let p = getAllQuestions().then(questions => questions.forEach(function (question) {
+    let p = getQuestion(question)
+      .then(function (data) {
+        let solutions = [];
+        if (data['data']['question']['solution'] != null) {
+          for (let uuid of parseSolutionURLs(data['data']['question']['solution']['content'])) {
+            let p = getSolution(uuid).then(function (data) {
+              if (data['data']['allPlaygroundCodes'] != null) {
+                data['data']['allPlaygroundCodes'].forEach(code =>
+                  solutions.push({code: code['code'], language: code['langSlug'], source: "official"}));
+              }
+            });
+            promises.push(p);
+          }
+        }
+        results.push({
+          questionId: data['data']['question']['questionId'],
+          titleSlug: data['data']['question']['titleSlug'],
+          solutions: solutions
+        });
+      });
+    promises.push(p);
+  }));
+  promises.push(p)
+  window.results = results;
+
+  // Save after all has been fetched
+  /* Broken
+  Promise.all(promises).then(function () {
+    filterResult();
+    saveResult();
   });
+  */
+
+  window.saveResult = function () {
+    filterResult();
+    saveResult();
+  };
 })(window);
